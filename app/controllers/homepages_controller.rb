ad168ab2c162dd8fa1e34590a3513d954394510a
RAIMUNDO <rvorellana@uc.cl>
2021-04-06 01:08:08 -0400

    bootstrap

--- app/controllers/homepages_controller.rb
+++ app/controllers/homepages_controller.rb
@@ -1,4 +1,7 @@
 class HomepagesController < ApplicationController
   def home
+    response = RestClient.get('https://tarea-1-breaking-bad.herokuapp.com/api/characters/1')
+    results = JSON.parse(response.to_str)
+    @name = results[0]["name"]
   end
 end
