e7fd7527ad5a3821099319a5b3da7dbc85d7bd4c
RAIMUNDO <rvorellana@uc.cl>
2021-04-06 00:27:16 -0400

    nuevo

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get "homepages/home", to: "homepages#home", as: 'home'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "homepages/home"
+  root :to => "homepages#home"
 end
