22ba8ba0ef8b5679bf3eb0cbb8061f7bfc5fe006
RAIMUNDO <rvorellana@uc.cl>
2021-04-06 00:20:52 -0400

    olaa

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get "homepages/home", to: "homepages#home", as: 'home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "homepages/home"
 end
